56317f60abd6e176c1c4684e8c17431f30ba8ade
Chris Schaefer <chris@dtzq.com>
2011-10-16 03:24:11 -0400

    Fixed "Use" section to reflect "-Dpackage" instead of "-DpackageName", fixed refs that were using artifactId and groupId with just the package variable.

--- src/main/resources/archetype-resources/src/main/java/__artifactId__/ui/web/controller/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * Package containing web controllers..
- * </p>
- */
-package $package.${artifactId}.ui.web.controller;
--- src/main/resources/archetype-resources/src/test/java/__artifactId__/ui/web/controller/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * Package containing web controller tests.
- * </p>
- */
-package $package.${artifactId}.ui.web.controller;
